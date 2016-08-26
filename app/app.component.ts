a22a1e83c7f819e93bbda599c414d68cd5ad3a52
Fernando Nunez <fernando.nunez@neyper.com>
2016-08-25 22:05:33 -0500

    Start nativescript-angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,23 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  template: `\n\
+    <StackLayout>\n\
+
+        <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+
+        <TextField hint="Email Address" keyboardType="email"
+          autocorrect="false" autocapitalizationType="none"></TextField>
+        <TextField hint="Password" secure="true"></TextField>
+
+        <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
+        <Button text="Sign up for Groceries"></Button>\n\
+    </StackLayout>
+  `,
+  styleUrls: ["pages/login/login-common.css","pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+    submit(){
+        console.log('hello');
+    }
+}
